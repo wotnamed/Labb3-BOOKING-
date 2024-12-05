5502e70e21591355a7c9111986ba2af84d6e21bd
wotnamed <sjogrenalvin@icloud.com>
2024-12-05 13:52:22 +0100

    Created backup json list (used list can be destroyed using write mode instead of read mode) etc.

---
+++ mainsys.py
@@ -0,0 +1,10 @@
+import json
+
+
+
+# json.dumps(data) dump string not file
+# youtube tutorial https://www.youtube.com/watch?v=-51jxlQaxyA
+with open("hotels.json", "r") as e:
+    hotel_list = json.load(e)
+
+print(hotel_list)
