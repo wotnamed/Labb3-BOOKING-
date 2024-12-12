f8a245a1d7d3ace382bf518d4ea54af3dab8254e
LukasEliasson <07lukeli@gafe.molndal.se>
2024-12-12 12:39:57 +0100

    ssssss

--- mainsys.py
+++ mainsys.py
@@ -1,10 +1,11 @@
 import json
 
+def get_data():
+    # json.dumps(data) dump string not file
+    # youtube tutorial https://www.youtube.com/watch?v=-51jxlQaxyA
+    with open("hotels.json", "r") as e:
+        hotel_list = json.load(e)
 
+    return hotel_list
 
-# json.dumps(data) dump string not file
-# youtube tutorial https://www.youtube.com/watch?v=-51jxlQaxyA
-with open("hotels.json", "r") as e:
-    hotel_list = json.load(e)
-
-print(hotel_list)
+get_data()
