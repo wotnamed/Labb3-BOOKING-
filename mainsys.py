8aa40520b9e8e5981c9b29116ce36713d14148e5
LukasEliasson <07lukeli@gafe.molndal.se>
2024-12-12 14:48:36 +0100

    ssssss

--- mainsys.py
+++ mainsys.py
@@ -1,13 +1,16 @@
 import json
 
-
 def get_data():
-    # json.dumps(data) dump string not file
-    # youtube tutorial https://www.youtube.com/watch?v=-51jxlQaxyA
     with open("hotels.json", "r") as e:
         hotel_list = json.load(e)
 
     return hotel_list
+
+def get_bookings():
+    with open("bookings.json", "r") as file:
+        bookings_list = json.load(file)
+
+    return bookings_list
 
 def display_hotels(hotels_data):
     for h in hotels_data:
@@ -28,10 +31,57 @@ def sort_hotels(hotels_data, k, reverse):
 
     return hotels_data
 
+def create_booking(hotels_data, bookings_data):
+    user = input("Name: ")
+    hotel = input("Hotel: ")
+    nights = int(input("Amount of nights: "))
+    rooms = int(input("Amount of rooms: "))
 
+    try:
+        hotel = list(filter(lambda hotels_data : hotels_data['name'].lower() == hotel.lower(), hotels_data))[0]
+    except IndexError:
+        return print('Hotel does not exist.')
+
+    total_cost = nights * rooms * hotel['cost_per_room']
+
+    if rooms > hotel['rooms_available']:
+        return print('Not enough rooms available.')
+
+    if (nights or rooms) == 0:
+        return print('You cannot book 0 nights or rooms.')
+
+    new_booking = {
+        "booking name": user,
+        "hotel": hotel['name'],
+        "nights booked": nights,
+        "rooms booked": rooms,
+        "total cost": total_cost
+    }
+
+    print(' ')
+    for e in new_booking:
+        print(f'{e.capitalize()}: {new_booking[e]}')
+    print(' ')
+    confirmation = input('Confirm booking? (yes/no): ')
+
+    if confirmation.lower() == "no":
+        return print('Cancelled booking.')
+
+    bookings_data.append(new_booking)
+
+    hotels_data[hotels_data.index(hotel)]['rooms_available'] -= rooms
+
+    with open("bookings.json", "w") as file:
+        json.dump(bookings_data, file, indent=4)
+
+    with open("hotels.json", "w") as file:
+        json.dump(hotels_data, file, indent=4)
+
+    print('Booking successfully created.')
 
 while True:
     data = get_data()
+    bookings = get_bookings()
     request = input()
 
     if request.lower() == "hotels":
@@ -45,3 +95,5 @@ while True:
 
         data = sort_hotels(data, key, order)
         display_hotels(data)
+    if request.lower() == "create booking":
+        create_booking(data, bookings)
